595377c51bb1f858326f765efd7aad17710e5eb3
sarangju <imagging@gmail.com>
2023-09-25 15:23:01 +0900

    week05 v1.0

--- week05/main.go
+++ week05/main.go
@@ -5,12 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n') // option 2, ignore  the errir return value with the
-	log.Fatal(err)
-	fmt.Println(inputScore)
+	inputScore, err := reader.ReadString('\n') // option 2
+	if err != nil {
+		log.Fatal(err)
+	}
+	inputScoreString = strings.TrimSpace(inputScoreString)      // remove space bar
+	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // casting
+	var grade string
+	if inputScore >= 90 {
+		grade := "A grade!"
+	} else {
+		grade := "under A grade~"
+	}
+	fmt.Println("You got " + grade) // 왜 에러남 자꾸
 }
