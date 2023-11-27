e61b1667af7494353c1fdba83fc2db6b8d581422
sarangju <imagging@gmail.com>
2023-11-27 14:38:28 +0900

    week14 v4.8

--- week14/main.go
+++ week14/main.go
@@ -3,20 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// map literal
-	games := map[int]string{
-		456: "우우우",
-		722: "최최최",
-		303: "정정정",
-		125: "김김김",
-		912: "유유유",
-		203: "왕왕왕", // 마지막에 쉼표 찍어줘야 ㅇㅇ
-	}
-
-	name, ok := games[722]
-	fmt.Println(name, ok)
-
-	for k, v := range games {
-		fmt.Println(k, v)
-	}
+	//   var balls map[string]int
+	balls := make(map[string]int)
+	fmt.Printf("%#v\n", balls)
+	balls["최최최"] = 777
+	fmt.Println(balls{"최최최"})
+	fmt.Println(balls{"없는사람"})
 }
