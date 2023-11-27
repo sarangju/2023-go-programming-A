02c955737ff80cbd360a130a6fca72e51769071c
sarangju <imagging@gmail.com>
2023-11-27 14:44:07 +0900

    week14 v4.9

--- week14/main.go
+++ week14/main.go
@@ -2,11 +2,23 @@ package main
 
 import "fmt"
 
+func status(name string) {
+	balls := map[string]int{"김": 10, "와": 0}
+	//ball := balls[name]
+	//fmt.Println(ball)
+	ball, exists := balls[name]
+
+	if !exists { // true면 ㅇㅇ false면 없
+		fmt.Println(name, "님은 게임 참여자가 아닙니다.")
+	} else if ball < 1 {
+		fmt.Println(name, "님은", balls[name], "개로 탈락했습니다")
+	} else {
+		fmt.Println(name, "님은 게임에서 승리하셨습니다.")
+	}
+}
+
 func main() {
-	//   var balls map[string]int
-	balls := make(map[string]int)
-	fmt.Printf("%#v\n", balls)
-	balls["최최최"] = 777
-	fmt.Println(balls{"최최최"})
-	fmt.Println(balls{"없는사람"})
+	status("김")
+	status("와")
+	status("왁")
 }
