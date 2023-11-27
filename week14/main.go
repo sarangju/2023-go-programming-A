eca0528c1eb01592963b48fbdaa9768ff5a74fe8
sarangju <imagging@gmail.com>
2023-11-27 14:15:34 +0900

    week14 v4.3

---
+++ week14/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var games map[int]string
+	games = make(map[int]string)
+
+	games[456] = "우우우"
+	games[722] = "최최최"
+	games[303] = "정정정"
+	games[125] = "김김김"
+	games[912] = "유유유"
+	games[203] = "왕왕왕"
+
+	for _, v := range games {
+		fmt.Println(v)
+	}
+
+}
