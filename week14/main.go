08b45ae12c3a123deb6883f5c80da16cac74665e
sarangju <imagging@gmail.com>
2023-11-27 14:32:01 +0900

    week14 v4.7

--- week14/main.go
+++ week14/main.go
@@ -3,22 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	
-	games:= map[int]string {
-		456:"우우우",
-		722:"최최최",
-		303:"정정정",
-		125:"김김김",
-		912:"유유유",
-		203:"왕왕왕," // 마지막에 쉼표 찍어줘야 ㅇㅇ
+	// map literal
+	games := map[int]string{
+		456: "우우우",
+		722: "최최최",
+		303: "정정정",
+		125: "김김김",
+		912: "유유유",
+		203: "왕왕왕", // 마지막에 쉼표 찍어줘야 ㅇㅇ
 	}
 
-	for _, v := range games {
-		fmt.Println(v)
-	}
-
-	games[456] = "곡곡곡" // update
-	delete(games, 912) // delete
+	name, ok := games[722]
+	fmt.Println(name, ok)
 
 	for k, v := range games {
 		fmt.Println(k, v)
